e49225a492181ba2c14732f7092eb8d94f42eb78
IsMeVit <ismevitt@gmail.com>
2025-10-27 18:58:44 +0700

    unanalytics from vercel

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar.vue';
 import About from './components/About.vue';
 import Project from './components/Project.vue';
 import Contact from './components/Contact.vue';
+
 </script>
 
 <template>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import './assets/font.css';
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 createApp(App).mount('#app')
