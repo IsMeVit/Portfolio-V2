a70c613dd3f05673be0a1efe0bc1c36b3cc88429
IsMeVit <ismevitt@gmail.com>
2025-10-27 18:56:56 +0700

    unanalytics from vercek

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <script setup>
-import { ref } from 'vue';
-import { track } from '@vercel/analytics';
-
 import Home from './components/Home.vue';
 import Background from './components/Background.vue';
 import Navbar from './components/Navbar.vue';
 import About from './components/About.vue';
 import Project from './components/Project.vue';
 import Contact from './components/Contact.vue';
-
-const currentView = ref('Home'); 
-
-function changeView(viewName) {
-  currentView.value = viewName;
-  
-  track('pageview', { page: `/${viewName}` }); 
-}
 </script>
 
 <template>
-  <div class="relative min-h-screen bg-[#1d2029] text-white flex flex-col items-center justify-center overflow-x-hidden ">
-    <Navbar @view-change="changeView"/>
+  <div class="relative min-h-screen bg-[#1d2029] text-white flex flex-col items-center justify-center overflow-x-hidden">
+    <Navbar/>
+    <Home/>
     <Background/>
-    <Home v-if="currentView === 'Home'"/>
-    <About v-else-if="currentView === 'About'"/>
-    <Project v-else-if="currentView === 'Project'"/>
-    <Contact v-else-if="currentView === 'Contact'"/>
+    <About/>
+    <Project/>
+    <Contact/>
 
     
 
--- src/main.js
+++ src/main.js
@@ -3,9 +3,5 @@ import './assets/font.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { inject } from "@vercel/analytics"
-
-inject();
 
 createApp(App).mount('#app')
-
